7a98c47c89f158d3dd5fcac7212065ad66096abf
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-05-03 22:39:48 -0700

    Create Generate_ID.sql

---
+++ SQL_Testing/Generate_ID.sql
@@ -0,0 +1,9 @@
+SELECT value FROM STRING_SPLIT('SP 1', ' ');
+/* */
+SELECT TOP 1 ma_san_pham  FROM SanPham   ORDER BY ma_san_pham DESC
+
+SELECT TOP 1 ma_san_pham FROM SanPham where ma_san_pham = @@
+SELECT NEWID()
+
+
+Select * FROM SanPham
