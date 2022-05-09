6f77cacdf8c6e4eea2c4481c1e804ec888b61c3c
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-05-09 11:11:27 +0700

    sữa lại sql thêm khóa ngoại

--- SQL_Testing/PERSISTED.sql
+++ SQL_Testing/PERSISTED.sql
@@ -4,7 +4,8 @@ use shop_laptop
 
 
 CREATE TABLE Student
-(StudentID AS 'ST' + CAST(AutoIncrement AS VARCHAR(8)) PERSISTED,
+(
+StudentID AS 'ST' + CAST(AutoIncrement AS VARCHAR(8)) PERSISTED,
  StudentName VARCHAR(20),
 AutoIncrement INT IDENTITY(1,1),
  CONSTRAINT PK_Student PRIMARY KEY(StudentID)
