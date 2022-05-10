f944677b25467ec7b54f03f7d8982916494d6394
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-05-10 07:23:08 +0700

    bacckup

---
+++ Database Backup and Import Data/query.sql
@@ -0,0 +1,4 @@
+SELECT * FROM hang_san_xuat
+
+INSERT INTO HangSanXuat(ten_hang_sx,logo) Select ten_hang_sx,logo FROM hang_san_xuat
+INSERT INTO HangSanXuat values ('DELL','dell.png')
--- SQL_Testing/Generate_ID.sql
+++
@@ -1,9 +0,0 @@
-SELECT value FROM STRING_SPLIT('SP 1', ' ');
-/* */
-SELECT TOP 1 ma_san_pham  FROM SanPham   ORDER BY ma_san_pham DESC
-
-SELECT TOP 1 ma_san_pham FROM SanPham where ma_san_pham = @@
-SELECT NEWID()
-
-
-Select * FROM SanPham
