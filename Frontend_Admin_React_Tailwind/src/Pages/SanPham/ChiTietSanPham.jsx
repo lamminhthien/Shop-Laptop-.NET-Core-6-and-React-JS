80d29b1aedb358a4596d40d06df7958450643cda
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-05-16 19:42:32 +0700

    backup detail page

--- Frontend_Admin_React_Tailwind/src/Pages/SanPham/ChiTietSanPham.jsx
+++ Frontend_Admin_React_Tailwind/src/Pages/SanPham/ChiTietSanPham.jsx
@@ -7,6 +7,41 @@ export default function ChiTietSanPham() {
   let { id } = useParams();
   let { path, url } = useRouteMatch();
 
+  // Local data to test front end
+  const data = [
+    {
+      id: 1, cpu: "ABcxaxsa", pin: 128, o_cung: 256,
+      he_dieu_hanh: "Windows 10", thiet_ke: "abcxyz",
+      kich_thuoc: "xyz",
+      mo_ta_them: "123"
+    },
+    {
+      id: 2, cpu: "ABcaaaaaaaaaaaaaaaaa", pin: 128, o_cung: 256,
+      he_dieu_hanh: "Windows 10", thiet_ke: "abcxyz",
+      kich_thuoc: "xyz",
+      mo_ta_them: "123"
+    },
+    {
+      id: 3, cpu: "ABc", pin: 128, o_cung: 256,
+      he_dieu_hanh: "Windows 10", thiet_ke: "abcxyz",
+      kich_thuoc: "xyz",
+      mo_ta_them: "123"
+    },
+    {
+      id: 4, cpu: "ABc", pin: 128, o_cung: 256,
+      he_dieu_hanh: "Windows 10", thiet_ke: "abcxyz",
+      kich_thuoc: "xyz",
+      mo_ta_them: "123"
+    },
+    {
+      id: 5, cpu: "ABc", pin: 128, o_cung: 256,
+      he_dieu_hanh: "Windows 10", thiet_ke: "abcxyz",
+      kich_thuoc: "xyz",
+      mo_ta_them: "123"
+    }
+  ]
+
+
   return (
     <div className="flex">
       <Sidebar />
@@ -14,6 +49,23 @@ export default function ChiTietSanPham() {
         <h3>ID: {id}</h3>
         <h4>Current path = {path}</h4>
         <h4>Current url = {url}</h4>
+        <div>
+          <table className="table-auto">
+            {data[id].cpu ?
+              <tr>
+                <th className="border-collapse bg-slate-200">
+                  Vi xử lý
+                </th>
+                <td className="pl-3 bg-slate-100">
+                  {data[id].cpu}
+                </td>
+              </tr>
+              : ""}
+
+
+
+          </table>
+        </div>
       </div>
 
     </div>
