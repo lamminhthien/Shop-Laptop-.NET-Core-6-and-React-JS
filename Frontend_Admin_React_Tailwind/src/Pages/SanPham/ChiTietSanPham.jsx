e4d80d78238ccbffb496023545b025e4f35cc1ec
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-05-17 11:24:34 +0700

    Update ChiTietSanPham.jsx

--- Frontend_Admin_React_Tailwind/src/Pages/SanPham/ChiTietSanPham.jsx
+++ Frontend_Admin_React_Tailwind/src/Pages/SanPham/ChiTietSanPham.jsx
@@ -60,135 +60,33 @@ export default function ChiTietSanPham() {
     <div className="flex">
       <Sidebar />
       <div className="h-screen flex-1 p-7">
-        <div class="bg-stripes-violet mt-2 flex gap-4 rounded-lg font-mono text-sm font-bold leading-6 text-white">
-          <div class="h-17  w-1/3 flex-none items-center justify-center rounded-lg bg-purple-300 p-4  dark:text-violet-400">
-
-            <div id="default-carousel" class="relative" data-carousel="static">
-
-              <div class="overflow-hidden relative h-56 rounded-lg sm:h-64 xl:h-80 2xl:h-96">
-
-                <div class="duration-700 ease-in-out absolute inset-0 transition-all transform translate-x-0 z-20" data-carousel-item="">
-                  <span class="absolute top-1/2 left-1/2 text-2xl font-semibold text-white -translate-x-1/2 -translate-y-1/2 sm:text-3xl dark:text-gray-800">First Slide</span>
-                  <img src={imgSRC[0]} class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="..." />
-                </div>
-
-                <div class="duration-700 ease-in-out absolute inset-0 transition-all transform translate-x-full z-10" data-carousel-item="">
-                  <img src={imgSRC[1]} class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="..." />
-                </div>
-
-                <div class="duration-700 ease-in-out absolute inset-0 transition-all transform -translate-x-full z-10" data-carousel-item="">
-                  <img src={imgSRC[2]} class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="..." />
-                </div>
-              </div>
-
-              <div class="flex absolute bottom-5 left-1/2 z-30 space-x-3 -translate-x-1/2">
-                <button type="button" class="w-3 h-3 rounded-full bg-white dark:bg-gray-800" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-                <button type="button" class="w-3 h-3 rounded-full bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-                <button type="button" class="w-3 h-3 rounded-full bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-              </div>
-
-              <button type="button" class="flex absolute top-0 left-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none" data-carousel-prev="">
-                <span class="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                  <svg class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
-                  <span class="hidden">Previous</span>
-                </span>
-              </button>
-              <button type="button" class="flex absolute top-0 right-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none" data-carousel-next="">
-                <span class="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                  <svg class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
-                  <span class="hidden">Next</span>
-                </span>
-              </button>
+        {/* Mã sản phẩm */}
+        <div class="grid grid-cols-4 shadow-2xl border-2 rounded-md">
+          <div class="flex items-center justify-center border align-middle">
+            <div class="m-2 inline-flex leading-3">
+              <svg class="mr-2 h-auto w-4 fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path></svg>
+              <h1>Mã sản phẩm</h1>
             </div>
-
           </div>
-          <div class="flex w-2/3 flex-auto items-center justify-center rounded-lg bg-purple-300 p-4 shadow-lg">
-            <table className=" text-sm text-left text-gray-500 dark:text-gray-400">
-              {data[id].cpu ?
-                <tr className="">
-                  <th className="border-collapse  bg-slate-200 px-6 py-3">
-                    Vi xử lý
-                  </th>
-                  <td className="pl-3 bg-slate-100 px-6 py-3">
-                    {data[id].cpu}
-                  </td>
-                </tr>
-                : ""}
-
-              {data[id].he_dieu_hanh ?
-                <tr>
-                  <th className="border-collapse bg-slate-200 px-6 py-3">
-                    Hệ điều hành
-                  </th>
-                  <td className="pl-3 bg-slate-100 px-6 py-3">
-                    {data[id].he_dieu_hanh}
-                  </td>
-                </tr>
-                : ""}
-
-              {data[id].kich_thuoc ?
-                <tr>
-                  <th className="border-collapse bg-slate-200 px-6 py-3">
-                    Kích thước
-                  </th>
-                  <td className="pl-3 bg-slate-100 px-6 py-3">
-                    {data[id].kich_thuoc}
-                  </td>
-                </tr>
-                : ""}
-
-              {data[id].mo_ta_them ?
-                <tr>
-                  <th className="border-collapse bg-slate-200 px-6 py-3">
-                    Mô tả thêm
-                  </th>
-                  <td className="pl-3 bg-slate-100 px-6 py-3">
-                    {data[id].mo_ta_them}
-                  </td>
-                </tr>
-                : ""}
-
-              {data[id].o_cung ?
-                <tr>
-                  <th className="border-collapse bg-slate-200 px-6 py-3">
-                    Ổ cứng
-                  </th>
-                  <td className="pl-3 bg-slate-100 px-6 py-3">
-                    {data[id].o_cung}
-                  </td>
-                </tr>
-                : ""}
-
-              {data[id].pin ?
-                <tr>
-                  <th className="border-collapse bg-slate-200 px-6 py-3">
-                    PIN
-                  </th>
-                  <td className="pl-3 bg-slate-100 px-6 py-3">
-                    {data[id].pin}
-                  </td>
-                </tr>
-                : ""}
-
-              {data[id].thiet_ke ?
-                <tr>
-                  <th className="border-collapse bg-slate-200 px-6 py-3">
-                    Th
-                  </th>
-                  <td className="pl-3 bg-slate-100 px-6 py-3">
-                    {data[id].thiet_ke}
-                  </td>
-                </tr>
-                : ""}
-
-
-
-            </table>
+          <div class="col-span-3 border   ">
+            <div class="px-3 leading-7">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
+          </div>
+          <div class="flex items-center justify-center border-x-2  align-middle">
+            <div class="m-2 inline-flex leading-3">
+              <svg class="mr-2 h-auto w-4 fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path></svg>
+              <h1>Mã sản phẩm</h1>
+            </div>
+          </div>
+          <div class="col-span-3 border  ">
+            <div class="px-3 leading-7">Lorem Ipsum is si</div>
           </div>
         </div>
+       
+
 
 
       </div>
+
       <script src="./node_modules/flowbite/dist/flowbite.js"></script>
     </div>
   );
