c039ca25102e3dfbd9ef88e88fb551551e01db61
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-05-17 13:10:08 +0700

    Update ChiTietSanPham.jsx

--- Frontend_Admin_React_Tailwind/src/Pages/SanPham/ChiTietSanPham.jsx
+++ Frontend_Admin_React_Tailwind/src/Pages/SanPham/ChiTietSanPham.jsx
@@ -63,7 +63,18 @@ export default function ChiTietSanPham() {
       <div className="h-screen flex-1 p-7">
 
         {/* // Khu vực thông tin chi tiết sản phẩm */}
-        <div class="grid grid-cols-4 shadow-2xl border rounded-2xl">
+        <div class="grid grid-cols-4 shadow-2xl border rounded-2xl bg-slate-300">
+          {/* Mô tả thêm */}
+          <div class="flex items-center justify-center border-r-2 border-b-2  align-middle ">
+            <div class="m-2 inline-flex leading-3">
+              <svg class="mr-2 h-auto w-4 fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path></svg>
+              <h1>Mô tả thêm</h1>
+            </div>
+          </div>
+          <div class="col-span-3 border-b-2 bg-white rounded-tr-2xl ">
+            <div class="px-3 leading-7 ">abc.</div>
+          </div>
+          {/*Kết thúc Mô tả thêm */}
           {/* Mô tả thêm */}
           <div class="flex items-center justify-center border-r-2 border-b-2  align-middle">
             <div class="m-2 inline-flex leading-3">
@@ -71,7 +82,7 @@ export default function ChiTietSanPham() {
               <h1>Mô tả thêm</h1>
             </div>
           </div>
-          <div class="col-span-3 border-b-2">
+          <div class="col-span-3 border-b-2 bg-white ">
             <div class="px-3 leading-7">abc.</div>
           </div>
           {/*Kết thúc Mô tả thêm */}
@@ -82,7 +93,29 @@ export default function ChiTietSanPham() {
               <h1>Mô tả thêm</h1>
             </div>
           </div>
-          <div class="col-span-3 border-b-2">
+          <div class="col-span-3 border-b-2 bg-white">
+            <div class="px-3 leading-7 ">abc.</div>
+          </div>
+          {/*Kết thúc Mô tả thêm */}
+          {/* Mô tả thêm */}
+          <div class="flex items-center justify-center border-r-2 border-b-2  align-middle">
+            <div class="m-2 inline-flex leading-3">
+              <svg class="mr-2 h-auto w-4 fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path></svg>
+              <h1>Mô tả thêm</h1>
+            </div>
+          </div>
+          <div class="col-span-3 border-b-2 bg-white">
+            <div class="px-3 leading-7 ">abc.</div>
+          </div>
+          {/*Kết thúc Mô tả thêm */}
+          {/* Mô tả thêm */}
+          <div class="flex items-center justify-center border-r-2 border-b-2  align-middle">
+            <div class="m-2 inline-flex leading-3">
+              <svg class="mr-2 h-auto w-4 fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path></svg>
+              <h1>Mô tả thêm</h1>
+            </div>
+          </div>
+          <div class="col-span-3 border-b-2 bg-white">
             <div class="px-3 leading-7">abc.</div>
           </div>
           {/*Kết thúc Mô tả thêm */}
@@ -93,47 +126,10 @@ export default function ChiTietSanPham() {
               <h1>Mô tả thêm</h1>
             </div>
           </div>
-          <div class="col-span-3 border-b-2">
+          <div class="col-span-3 border-b-2 bg-white rounded-br-xl">
             <div class="px-3 leading-7">abc.</div>
           </div>
           {/*Kết thúc Mô tả thêm */}
-          {/* Mô tả thêm */}
-          <div class="flex items-center justify-center border-r-2 border-b-2  align-middle">
-            <div class="m-2 inline-flex leading-3">
-              <svg class="mr-2 h-auto w-4 fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path></svg>
-              <h1>Mô tả thêm</h1>
-            </div>
-          </div>
-          <div class="col-span-3 border-b-2">
-            <div class="px-3 leading-7">abc.</div>
-          </div>
-          {/*Kết thúc Mô tả thêm */}
-          {/* Mô tả thêm */}
-          <div class="flex items-center justify-center border-r-2 border-b-2  align-middle">
-            <div class="m-2 inline-flex leading-3">
-              <svg class="mr-2 h-auto w-4 fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path></svg>
-              <h1>Mô tả thêm</h1>
-            </div>
-          </div>
-          <div class="col-span-3 border-b-2">
-            <div class="px-3 leading-7">abc.</div>
-          </div>
-          {/*Kết thúc Mô tả thêm */}
-          {/* Mô tả thêm */}
-          <div class="flex items-center justify-center border-r-2 border-b-2  align-middle">
-            <div class="m-2 inline-flex leading-3">
-              <svg class="mr-2 h-auto w-4 fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path></svg>
-              <h1>Mô tả thêm</h1>
-            </div>
-          </div>
-          <div class="col-span-3 border-b-2">
-            <div class="px-3 leading-7">abc.</div>
-          </div>
-          {/*Kết thúc Mô tả thêm */}
-
-         
-
-
 
         </div>
         {/* //Kết thúc  Khu vực thông tin chi tiết sản phẩm */}
