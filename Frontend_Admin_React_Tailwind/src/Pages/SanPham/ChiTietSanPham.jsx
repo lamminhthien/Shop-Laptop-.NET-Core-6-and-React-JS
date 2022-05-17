ecf5d2427d1b98ed5f4526e7be11988eeac4cf86
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-05-17 11:58:17 +0700

    backup giao diện

--- Frontend_Admin_React_Tailwind/src/Pages/SanPham/ChiTietSanPham.jsx
+++ Frontend_Admin_React_Tailwind/src/Pages/SanPham/ChiTietSanPham.jsx
@@ -28,7 +28,7 @@ export default function ChiTietSanPham() {
       id: 2, cpu: "ABcaaaaaaaaaaaaaaaaa", pin: 128, o_cung: 256,
       he_dieu_hanh: "Windows 10", thiet_ke: "abcxyz",
       kich_thuoc: "xyz",
-      mo_ta_them: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+      mo_ta_them: "abc."
     },
     {
       id: 3, cpu: "ABc", pin: 128, o_cung: 256,
@@ -59,29 +59,85 @@ export default function ChiTietSanPham() {
   return (
     <div className="flex">
       <Sidebar />
+
       <div className="h-screen flex-1 p-7">
-        {/* Mã sản phẩm */}
-        <div class="grid grid-cols-4 shadow-2xl border-2 rounded-md">
-          <div class="flex items-center justify-center border align-middle">
+
+        {/* // Khu vực thông tin chi tiết sản phẩm */}
+        <div class="grid grid-cols-4 shadow-2xl border rounded-2xl">
+          {/* Mô tả thêm */}
+          <div class="flex items-center justify-center border-r-2 border-b-2  align-middle">
             <div class="m-2 inline-flex leading-3">
               <svg class="mr-2 h-auto w-4 fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path></svg>
-              <h1>Mã sản phẩm</h1>
+              <h1>Mô tả thêm</h1>
             </div>
           </div>
-          <div class="col-span-3 border   ">
-            <div class="px-3 leading-7">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
+          <div class="col-span-3 border-b-2">
+            <div class="px-3 leading-7">abc.</div>
           </div>
-          <div class="flex items-center justify-center border-x-2  align-middle">
+          {/*Kết thúc Mô tả thêm */}
+          {/* Mô tả thêm */}
+          <div class="flex items-center justify-center border-r-2 border-b-2  align-middle">
             <div class="m-2 inline-flex leading-3">
               <svg class="mr-2 h-auto w-4 fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path></svg>
-              <h1>Mã sản phẩm</h1>
+              <h1>Mô tả thêm</h1>
             </div>
           </div>
-          <div class="col-span-3 border  ">
-            <div class="px-3 leading-7">Lorem Ipsum is si</div>
+          <div class="col-span-3 border-b-2">
+            <div class="px-3 leading-7">abc.</div>
           </div>
+          {/*Kết thúc Mô tả thêm */}
+          {/* Mô tả thêm */}
+          <div class="flex items-center justify-center border-r-2 border-b-2  align-middle">
+            <div class="m-2 inline-flex leading-3">
+              <svg class="mr-2 h-auto w-4 fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path></svg>
+              <h1>Mô tả thêm</h1>
+            </div>
+          </div>
+          <div class="col-span-3 border-b-2">
+            <div class="px-3 leading-7">abc.</div>
+          </div>
+          {/*Kết thúc Mô tả thêm */}
+          {/* Mô tả thêm */}
+          <div class="flex items-center justify-center border-r-2 border-b-2  align-middle">
+            <div class="m-2 inline-flex leading-3">
+              <svg class="mr-2 h-auto w-4 fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path></svg>
+              <h1>Mô tả thêm</h1>
+            </div>
+          </div>
+          <div class="col-span-3 border-b-2">
+            <div class="px-3 leading-7">abc.</div>
+          </div>
+          {/*Kết thúc Mô tả thêm */}
+          {/* Mô tả thêm */}
+          <div class="flex items-center justify-center border-r-2 border-b-2  align-middle">
+            <div class="m-2 inline-flex leading-3">
+              <svg class="mr-2 h-auto w-4 fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path></svg>
+              <h1>Mô tả thêm</h1>
+            </div>
+          </div>
+          <div class="col-span-3 border-b-2">
+            <div class="px-3 leading-7">abc.</div>
+          </div>
+          {/*Kết thúc Mô tả thêm */}
+          {/* Mô tả thêm */}
+          <div class="flex items-center justify-center border-r-2 border-b-2  align-middle">
+            <div class="m-2 inline-flex leading-3">
+              <svg class="mr-2 h-auto w-4 fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path></svg>
+              <h1>Mô tả thêm</h1>
+            </div>
+          </div>
+          <div class="col-span-3 border-b-2">
+            <div class="px-3 leading-7">abc.</div>
+          </div>
+          {/*Kết thúc Mô tả thêm */}
+
+         
+
+
+
         </div>
-       
+        {/* //Kết thúc  Khu vực thông tin chi tiết sản phẩm */}
+
 
 
 
