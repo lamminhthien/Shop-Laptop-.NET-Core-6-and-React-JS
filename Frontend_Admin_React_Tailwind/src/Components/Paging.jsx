fb88c066d33ca2f081f764113018c3f209438222
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-05-19 17:20:31 +0700

    humm, fix nhẹ

--- Frontend_Admin_React_Tailwind/src/Components/Paging.jsx
+++ Frontend_Admin_React_Tailwind/src/Components/Paging.jsx
@@ -12,7 +12,7 @@ export default function Paging(props) {
                 </li>
                 {Array.from({ length: props.pages }, (val, ind) =>
                     <li>
-                        <a href={ind.toString()} class="py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">{ind+1}</a>
+                        <a href={(ind+1).toString()} class="py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">{ind+1}</a>
                     </li>
                 )}
                 <li>
