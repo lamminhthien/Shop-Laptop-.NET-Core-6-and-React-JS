11c60cbf4cc42e6d529d3fe4d8563d6807a3a4a5
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-05-21 22:22:05 +0700

    history.goBack()

--- Frontend_Admin_React_Tailwind/src/Pages/SanPham/XoaSanPham.jsx
+++ Frontend_Admin_React_Tailwind/src/Pages/SanPham/XoaSanPham.jsx
@@ -1,21 +1,26 @@
 import { useParams, useRouteMatch } from "react-router-dom";
+import { useHistory } from 'react-router-dom'
 import axios from "axios";
 
 
 export default function XoaSanPham() {
   // the dynamic pieces of the URL.
   let { id } = useParams();
-  alert(id)
+  const history = useHistory()
 
     axios.delete(`https://localhost:7216/api/QuanLySanPham/XoaSanPham/${id}`)
     .then(
         () => {
             alert("Delete complete")
+            // Điều hướng về lại trang trước
+            history.goBack()
         }
     )
     .catch(
         () => {
             alert("Error" + id)
+            // Điều hướng về lại trang trước
+            history.goBack()
         }
     )
 }
