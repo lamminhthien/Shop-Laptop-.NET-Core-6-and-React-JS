bf1de1479a79377cd10bf74014e3ff55747484cf
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-05-28 14:25:49 +0700

    backuo

--- Backend_NETCore_EFCore/Controllers/PublicController/TrangChuController.cs
+++ Backend_NETCore_EFCore/Controllers/PublicController/TrangChuController.cs
@@ -23,12 +23,34 @@ namespace ShopLaptop_EFCore.Controllers.PublicController
             var listLoaiSanPham = _context.LoaiSanPhams.Select(
                    o => new
                    {
+                       maLoaiSanPham = o.MaLoaiSp,
                        tenLoaiSanPham = o.TenLoaiSp,
-                       anhLoaiSanPham = imageURL + o.AnhMinhHoa
+                       anhLoaiSanPham = imageURL + o.AnhMinhHoa,
                    }
                 );
 
             return Ok(listLoaiSanPham);
+        }   
+
+        // Lọc danh sách sản phẩm theo yêu cầu
+        [HttpGet("SanPhamByCategory")]
+        public ActionResult<List<dynamic>> SanPhamByCategory(int id)
+        {
+            var imageURL = Request.Scheme + "://" + Request.Host.Value + "/" + "Resources/Images/SanPham/";
+            //var listSanPham = from a in _context.SanPhams
+            //                  join b in _context.ChiTietSanPhams
+            //                  on a.MaSanPham equals b.MaSanPham
+            //                  where a.MaLoaiSp == id
+            //                  select new
+            //                  {
+            //                     tenSanPham =  a.TenSanPham,
+            //                      giaNiemYet = (from a)
+            //                  }
+            var test = from c in _context.BienDongGia
+                        where c.MaSanPham == 1
+                        select c;
+            return Ok(test);
+
         }
 
      
