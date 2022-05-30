ee41227562ab186def3dfb611e55a9fa7f9c6af8
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-05-30 22:34:26 +0700

    Update BrandList.jsx

--- Frontend_React_Tailwind/src/Components/Public/BrandList.jsx
+++ Frontend_React_Tailwind/src/Components/Public/BrandList.jsx
@@ -19,24 +19,25 @@ export default function BrandList() {
     }, [])
     if (state.done)
         return (
-            <div className="my-16">
-                <a href="#" class="flex flex-wrap place-content-center overflow-hidden">
-                    {state.data !== null ? state.data.map((item) =>
-                        <a href="#">
-                            <div className="w-60 p-2 bg-white rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl">
-
-
-                                {/* Image */}
-                                <img className="h-40 object-cover rounded-xl h-40 object-cover rounded-xl" src={item.logo} alt />
-                                <div className="p-2">
-                                    {/* Heading */}
-                                    <h2 className="font-bold text-lg mb-2 ">{item.tenHangSanXuat}</h2>
+            <div>
+                 <h1 className="text-center text-fuchsia-500  text-3xl font-semibold">Hãng sản xuất</h1>
+                <div className="my-16">
+                    <a href="#" class="flex flex-wrap place-content-center overflow-hidden">
+                        {state.data !== null ? state.data.map((item) =>
+                            <a href="#">
+                                <div className="w-60 p-2 bg-white rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl">
+                                    {/* Image */}
+                                    <img className="h-40 object-cover rounded-xl h-40 object-cover rounded-xl" src={item.logo} alt />
+                                    <div className="p-2">
+                                        {/* Heading */}
+                                        <h2 className="font-bold text-lg mb-2 ">{item.tenHangSanXuat}</h2>
+                                    </div>
+                                    {/* CTA */}
                                 </div>
-                                {/* CTA */}
-                            </div>
-                        </a>
-                    ) : <p>Đang load data</p>}
-                </a>
+                            </a>
+                        ) : <p>Đang load data</p>}
+                    </a>
+                </div>
             </div>
         )
 
