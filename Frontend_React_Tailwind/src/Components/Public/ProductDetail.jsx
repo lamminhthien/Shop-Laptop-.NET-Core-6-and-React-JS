baff4ca5fa48a81d4db8ddc67b4359d3486cf0a7
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-05-30 20:41:06 +0700

    Update ProductDetail.jsx

--- Frontend_React_Tailwind/src/Components/Public/ProductDetail.jsx
+++ Frontend_React_Tailwind/src/Components/Public/ProductDetail.jsx
@@ -27,7 +27,7 @@ export default function ProductDetail() {
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-row">
             <div className="lg:basis-2/4">
-              <img alt="ecommerce" className="h-fit w-fit object-cover object-center rounded" src={state.image[1]} />
+              <img alt="ecommerce" className="mt-auto h-3/5  w-fit object-cover object-center rounded" src={state.image[1]} />
               <div className="flex flex-row  justify-center space-x-4 font-mono text-white text-sm font-bold leading-6">
               {state.image !== null ? state.image.map((item) =>
                 <img alt="ecommerce" className="w-1/4 object-cover object-center rounded" src={item} />
@@ -59,38 +59,9 @@ export default function ProductDetail() {
                   <li>Mô tả thêm: {state.data.moTaThem}</li>
                 </ul>
               </div>
-              <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
-                <div className="flex">
-                  <span className="mr-3">Color</span>
-                  <button className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none" />
-                  <button className="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none" />
-                  <button className="border-2 border-gray-300 ml-1 bg-indigo-500 rounded-full w-6 h-6 focus:outline-none" />
-                </div>
-                <div className="flex ml-6 items-center">
-                  <span className="mr-3">Size</span>
-                  <div className="relative">
-                    <select className="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
-                      <option>SM</option>
-                      <option>M</option>
-                      <option>L</option>
-                      <option>XL</option>
-                    </select>
-                    <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
-                      <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-4 h-4" viewBox="0 0 24 24">
-                        <path d="M6 9l6 6 6-6" />
-                      </svg>
-                    </span>
-                  </div>
-                </div>
-              </div>
               <div className="flex">
                 <span className="title-font font-medium text-2xl text-gray-900">Giá: {state.data.giaNiemYet} VND</span>
                 <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Thêm vào giỏ hàng</button>
-                <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-                  <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-5 h-5" viewBox="0 0 24 24">
-                    <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z" />
-                  </svg>
-                </button>
               </div>
             </div>
           </div>
