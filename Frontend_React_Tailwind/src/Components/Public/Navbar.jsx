5efa485f7d051b4703d55f244398e01d85918e9d
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-05-31 23:00:10 +0700

    Update Navbar.jsx

--- Frontend_React_Tailwind/src/Components/Public/Navbar.jsx
+++ Frontend_React_Tailwind/src/Components/Public/Navbar.jsx
@@ -15,7 +15,7 @@ export default function Navbar() {
           <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" /></svg>
         </button>
         <div className="hidden w-full md:block md:w-auto" id="mobile-menu">
-          <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+          <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-3 md:text-sm md:font-medium">
             <li>
               <a href="#" className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Trang chủ</a>
             </li>
@@ -32,7 +32,7 @@ export default function Navbar() {
               <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0  ">Giỏ hàng</a>
             </li>
             <li>
-              <form>
+              <form className="mt-[-3px] w-max">
                 <input type="text"
                   {...register("searchKey")}
                   class=" text-gray-900 bg-gray-50 rounded-lg border
