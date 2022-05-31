c2d1dc892ffd3ea5fad399f3a0ce283d63613791
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-05-31 22:50:32 +0700

    Update ProductList.jsx

--- Frontend_React_Tailwind/src/Components/Public/ProductList.jsx
+++ Frontend_React_Tailwind/src/Components/Public/ProductList.jsx
@@ -93,9 +93,8 @@ export default function ProductList() {
                 </a>
 
                 <div className="mt-4">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-                  <p className="mt-1">$16.00</p>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">{item.tenSanPham}</h2>
+                  <p className="mt-1">{item.giaNiemYet} VND</p>
                 </div>
               </div>
             )}
