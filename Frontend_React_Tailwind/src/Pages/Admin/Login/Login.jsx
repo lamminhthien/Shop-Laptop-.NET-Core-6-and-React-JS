af6d8b27a0d7de1cdf86fc6f3df126f931ce0025
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-06-02 18:14:34 +0700

    check jwt token and redirect to ListSanPham in Login

--- Frontend_React_Tailwind/src/Pages/Admin/Login/Login.jsx
+++ Frontend_React_Tailwind/src/Pages/Admin/Login/Login.jsx
@@ -1,69 +1,62 @@
 import axios from 'axios';
 import LoginJWT from '../../../Api/LoginJWT';
-
+import authorizedAdmin from '../../../Helpers/authenticationAdmin.js';
+import ListSanPham from '../SanPham/ListSanPham';
 export default function LoginCreateJWT() {
-
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
-    const username = e.target[0].value
-    const password = e.target[1].value
-    LoginJWT(username,password)
-  }
-
+    const username = e.target[0].value;
+    const password = e.target[1].value;
+    LoginJWT(username, password);
+  };
 
   // Render form
-  return (
-    <div class="body-bg min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0 bg-gradient-to-r
+  // If admin, manager don't have token,render login form
+  if (!authorizedAdmin) {
+    return (
+      <div
+        class='body-bg min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0 bg-gradient-to-r
       from-indigo-600
-      to-blue-400">
+      to-blue-400'>
+        <main class='bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl'>
+          <section>
+            <h3 class='font-bold text-2xl text-center'>Chào mừng nhân viên Đức Thịnh Laptop</h3>
+          </section>
 
-      <main class="bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
-        <section>
-          <h3 class="font-bold text-2xl text-center">Chào mừng nhân viên Đức Thịnh Laptop</h3>
-        </section>
-
-        <section class="mt-10">
-          <form class="flex flex-col" method="POST" action="#" onSubmit={(e) => handleSubmit(e)}>
-            <div class="mb-6 pt-3 rounded bg-gray-200">
-              <label
-                class="block text-gray-700 text-sm font-bold mb-2 ml-3"
-                for="email"
-              >
-                Username
-              </label>
-              <input
-                type="text"
-                name="Username"
-                class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
-
-              />
-            </div>
-            <div class="mb-6 pt-3 rounded bg-gray-200">
-              <label
-                class="block text-gray-700 text-sm font-bold mb-2 ml-3"
-                for="password"
-              >
-                Mật khẩu
-              </label>
-              <input
-                type="password"
-                name="Password"
-                class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
-              />
-            </div>
-            <button
-              class="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200"
-              type="submit"
-            >
-              Đăng nhập
-            </button>
-          </form>
-        </section>
-      </main>
-
-
-
-
-    </div>
+          <section class='mt-10'>
+            <form class='flex flex-col' method='POST' action='#' onSubmit={e => handleSubmit(e)}>
+              <div class='mb-6 pt-3 rounded bg-gray-200'>
+                <label class='block text-gray-700 text-sm font-bold mb-2 ml-3' for='email'>
+                  Username
+                </label>
+                <input
+                  type='text'
+                  name='Username'
+                  class='bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3'
+                />
+              </div>
+              <div class='mb-6 pt-3 rounded bg-gray-200'>
+                <label class='block text-gray-700 text-sm font-bold mb-2 ml-3' for='password'>
+                  Mật khẩu
+                </label>
+                <input
+                  type='password'
+                  name='Password'
+                  class='bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3'
+                />
+              </div>
+              <button
+                class='bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200'
+                type='submit'>
+                Đăng nhập
+              </button>
+            </form>
+          </section>
+        </main>
+      </div>
+    );
+  }
+  else return (
+    <ListSanPham/>
   )
 }
