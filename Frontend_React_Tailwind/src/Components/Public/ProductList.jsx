df79534da50118194ff46db451d2b158df39040d
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-06-07 06:44:17 +0700

    fix query para

--- Frontend_React_Tailwind/src/Components/Public/ProductList.jsx
+++ Frontend_React_Tailwind/src/Components/Public/ProductList.jsx
@@ -13,32 +13,34 @@ export default function ProductList() {
   }
   let query = useQuery();
   var page = 1;
-  const [listProduct, setListProduct] = useState([]); 
+  const [listProduct, setListProduct] = useState([]);
   const [pageState, setPageState] = useState(0);
   // Query handle
   const queryHandler = () => {
-    const formData = new FormData()
-    let brand = query.get('brand')
-    let category = query.get('category')
-    let priceMin = query.get('price-min')
-    let priceMax = query.get('price-max')
-    let page = query.get('page')
-    if (!isNaN(brand)) formData.append("maHangSanXuat",brand)
-    if (!isNaN(category)) formData.append("maLoaiSanPham",category)
-    if (!isNaN(priceMin)) formData.append("minPrice",priceMin)
-    if (!isNaN(priceMax)) formData.append("maxPrice",priceMax)
-    if (!isNaN(page)) formData.append("page",page)
-    let params=`?`
+    const formData = new FormData();
+    let brand = query.get('brand');
+    let category = query.get('category');
+    let priceMin = query.get('price-min');
+    let priceMax = query.get('price-max');
+    let page = query.get('page');
+    if (!isNaN(brand)) formData.append('maHangSanXuat', brand);
+    if (!isNaN(category)) formData.append('maLoaiSanPham', category);
+    if (!isNaN(priceMin)) formData.append('minPrice', priceMin);
+    if (!isNaN(priceMax)) formData.append('maxPrice', priceMax);
+    if (!isNaN(page)) formData.append('page', page);
+    let params = `?`;
     for (const pair of formData.entries()) {
-      params = params + `${pair[0]}=${pair[1]}` + '&'
+      if (!isNaN(pair[1])) params = params + `${pair[0]}=${pair[1]}` + '&'; 
     }
-    params = params.slice(0,params.length -1)
-    TrangChuApi.getSanPhamByAdvanceSearch(params).then(res => {
-      setPageState(1)
-    }).catch(err => {
-      setPageState(-1)
-    })
-  }
+    params = params.slice(0, params.length - 1);
+    TrangChuApi.getSanPhamByAdvanceSearch(params)
+      .then(res => {
+        setPageState(1);
+      })
+      .catch(err => {
+        setPageState(-1);
+      });
+  };
   // SetState in react-hook
   useEffect(() => {
     TrangChuApi.getSanPhamByDefault(parseInt(page))
@@ -51,46 +53,46 @@ export default function ProductList() {
       });
   }, []);
   //Excute queryHandler()
-  queryHandler()
+  queryHandler();
   // Page Renderring Item
   const pageRender = () => {
     if (pageState === 0) return <p className='text-2xl'>Đang tải dữ liệu</p>;
     if (pageState === -1) return <p className='text-red-500 text-2xl'>Trang bạn yêu cầu có lỗi</p>;
     return (
-        // <div className='container mt-5'>
-        //   <div className='flex flex-wrap justify-start'>
-        //     {listProduct.data.ketQua.map(item => (
-        //       <a
-        //         href={item.maSanPham}
-        //         className='h-fit
-        //             bg-white shadow-lg
-        //             rounded-md border-black border-0 w-60 flex
-        //             flex-wrap justify-center mx-4 my-4
-        //             '>
-        //         <div
-        //           className='
-        //         max-h-40 bg-center px-3 py-3
-        //         '>
-        //           <img src='https://vatvostudio.vn/wp-content/uploads/2022/04/iPhone-12-hang-Nhat-ma-ja-ve-Viet-Nam-2.jpg' />
-        //         </div>
-        //         <p
-        //           className='
-        //             font-bold text-sm text-black w-full text-center my-3
-        //           '>
-        //           {item.tenSanPham}
-        //         </p>
-        //         <div
-        //           className='
-        //         bg-orange-500 w-fit px-4 text-white
-        //          border-orange-500 rounded-2xl py-3
-        //         '>
-        //           {item.giaNiemYet}đ
-        //         </div>
-        //       </a>
-        //     ))}
-        //   </div>
-        // </div>
-          <>RenderArea</>
+      // <div className='container mt-5'>
+      //   <div className='flex flex-wrap justify-start'>
+      //     {listProduct.data.ketQua.map(item => (
+      //       <a
+      //         href={item.maSanPham}
+      //         className='h-fit
+      //             bg-white shadow-lg
+      //             rounded-md border-black border-0 w-60 flex
+      //             flex-wrap justify-center mx-4 my-4
+      //             '>
+      //         <div
+      //           className='
+      //         max-h-40 bg-center px-3 py-3
+      //         '>
+      //           <img src='https://vatvostudio.vn/wp-content/uploads/2022/04/iPhone-12-hang-Nhat-ma-ja-ve-Viet-Nam-2.jpg' />
+      //         </div>
+      //         <p
+      //           className='
+      //             font-bold text-sm text-black w-full text-center my-3
+      //           '>
+      //           {item.tenSanPham}
+      //         </p>
+      //         <div
+      //           className='
+      //         bg-orange-500 w-fit px-4 text-white
+      //          border-orange-500 rounded-2xl py-3
+      //         '>
+      //           {item.giaNiemYet}đ
+      //         </div>
+      //       </a>
+      //     ))}
+      //   </div>
+      // </div>
+      <>RenderArea</>
     );
   };
   return <>{pageRender()}</>;
