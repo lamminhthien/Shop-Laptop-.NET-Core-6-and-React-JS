97548b65e3c85b164ab82f2d93b911811e829506
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-06-06 21:47:24 +0700

    Update ProductList.jsx

--- Frontend_React_Tailwind/src/Components/Public/ProductList.jsx
+++ Frontend_React_Tailwind/src/Components/Public/ProductList.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import TrangChuApi from '../../Api/Public/TrangChuApi';
 import { useState, useEffect } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
@@ -12,17 +13,42 @@ export default function ProductList() {
   const [listProduct, setListProduct] = useState([]);
   const [pageState, setPageState] = useState(0);
   useEffect(() => {
-    TrangChuApi.getSanPhamByDefault(parseInt(page)).then(res => {
-      setPageState(1)
-      setListProduct(res)
-    }).catch(err => {
-      setPageState(-1)
-    })
+    TrangChuApi.getSanPhamByDefault(parseInt(page))
+      .then(res => {
+        setListProduct(res);
+        setPageState(1);
+      })
+      .catch(err => {
+        setPageState(-1);
+      });
   }, []);
   const pageRender = () => {
     if (pageState === 0) return <p className='text-2xl'>Đang tải dữ liệu</p>;
     if (pageState === -1) return <p className='text-red-500 text-2xl'>Trang bạn yêu cầu có lỗi</p>;
-    return <>{JSON.stringify(listProduct)}</>
+    return (
+      <>
+        {JSON.stringify(listProduct)}
+        {/* {/* <>----------This is map function--------</> <br /> */}
+        {listProduct.data.ketQua.map(item => (
+          <div
+            className='
+      bg-white box-border shadow-lg 
+      rounded-md shadow-fuchsia-400 w-60'>
+            <h3>
+              <a href='#'
+                className='
+                font-bold text-sm text-black
+              '>
+                {item.tenSanPham}
+              </a>
+            </h3>
+            <p>{item.giaNiemYet}</p>
+          </div>
+        ))} */
+        <>-------------------f--</> <br />
+        <>{JSON.stringify(listProduct.data.ketQua)}</>
+      </>
+    );
   };
   return <>{pageRender()}</>;
 }
