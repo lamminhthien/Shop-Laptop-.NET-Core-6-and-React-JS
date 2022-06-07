39fc9ebdd01cee047d3dc03c0f3dcd677eacaabb
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-06-07 07:01:44 +0700

    fix thứ tự setState

--- Frontend_React_Tailwind/src/Components/Public/ProductList.jsx
+++ Frontend_React_Tailwind/src/Components/Public/ProductList.jsx
@@ -30,11 +30,12 @@ export default function ProductList() {
     if (!isNaN(page)) formData.append('page', page);
     let params = `?`;
     for (const pair of formData.entries()) {
-      if (!isNaN(pair[1])) params = params + `${pair[0]}=${pair[1]}` + '&'; 
+      if (!isNaN(pair[1])) params = params + `${pair[0]}=${pair[1]}` + '&';
     }
     params = params.slice(0, params.length - 1);
     TrangChuApi.getSanPhamByAdvanceSearch(params)
       .then(res => {
+        setListProduct(res);
         setPageState(1);
       })
       .catch(err => {
@@ -43,57 +44,50 @@ export default function ProductList() {
   };
   // SetState in react-hook
   useEffect(() => {
-    TrangChuApi.getSanPhamByDefault(parseInt(page))
-      .then(res => {
-        setListProduct(res);
-        setPageState(1);
-      })
-      .catch(err => {
-        setPageState(-1);
-      });
+    //Excute queryHandler()
+    queryHandler();
   }, []);
-  //Excute queryHandler()
-  queryHandler();
   // Page Renderring Item
   const pageRender = () => {
     if (pageState === 0) return <p className='text-2xl'>Đang tải dữ liệu</p>;
     if (pageState === -1) return <p className='text-red-500 text-2xl'>Trang bạn yêu cầu có lỗi</p>;
-    return (
-      // <div className='container mt-5'>
-      //   <div className='flex flex-wrap justify-start'>
-      //     {listProduct.data.ketQua.map(item => (
-      //       <a
-      //         href={item.maSanPham}
-      //         className='h-fit
-      //             bg-white shadow-lg
-      //             rounded-md border-black border-0 w-60 flex
-      //             flex-wrap justify-center mx-4 my-4
-      //             '>
-      //         <div
-      //           className='
-      //         max-h-40 bg-center px-3 py-3
-      //         '>
-      //           <img src='https://vatvostudio.vn/wp-content/uploads/2022/04/iPhone-12-hang-Nhat-ma-ja-ve-Viet-Nam-2.jpg' />
-      //         </div>
-      //         <p
-      //           className='
-      //             font-bold text-sm text-black w-full text-center my-3
-      //           '>
-      //           {item.tenSanPham}
-      //         </p>
-      //         <div
-      //           className='
-      //         bg-orange-500 w-fit px-4 text-white
-      //          border-orange-500 rounded-2xl py-3
-      //         '>
-      //           {item.giaNiemYet}đ
-      //         </div>
-      //       </a>
-      //     ))}
-      //   </div>
-      // </div>
-      <>RenderArea</>
-    );
+    if (pageState === 1)
+      return (
+        // <div className='container mt-5'>
+        //   <div className='flex flex-wrap justify-start'>
+        //     {listProduct.data.ketqua.map(item => (
+        //       <a
+        //         href={item.maSanPham}
+        //         className='h-fit
+        //             bg-white shadow-lg
+        //             rounded-md border-black border-0 w-60 flex
+        //             flex-wrap justify-center mx-4 my-4
+        //             '>
+        //         <div
+        //           className='
+        //         max-h-40 bg-center px-3 py-3
+        //         '>
+        //           <img src='https://vatvostudio.vn/wp-content/uploads/2022/04/iPhone-12-hang-Nhat-ma-ja-ve-Viet-Nam-2.jpg' />
+        //         </div>
+        //         <p
+        //           className='
+        //             font-bold text-sm text-black w-full text-center my-3
+        //           '>
+        //           {item.tenSanPham}
+        //         </p>
+        //         <div
+        //           className='
+        //         bg-orange-500 w-fit px-4 text-white
+        //          border-orange-500 rounded-2xl py-3
+        //         '>
+        //           {item.giaNiemYet}đ
+        //         </div>
+        //       </a>
+        //     ))}
+        //   </div>
+        // </div>
+        <>{JSON.stringify(listProduct.data.ketqua)}</>
+      );
   };
   return <>{pageRender()}</>;
 }
