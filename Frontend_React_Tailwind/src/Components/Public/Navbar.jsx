aa204714884ec89e3dfbf7e6a8cc70c741341127
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-06-07 21:35:12 +0700

    tạo nút đăng xuất

--- Frontend_React_Tailwind/src/Components/Public/Navbar.jsx
+++ Frontend_React_Tailwind/src/Components/Public/Navbar.jsx
@@ -52,6 +52,12 @@ export default function Navbar() {
               Xin chào {userName}
             </p>
           </li>
+          <li>
+            <a href="/" onClick={()=>{localStorage.removeItem('token')}}
+              className='block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 '>
+              Đăng xuất
+            </a>
+          </li>
         </>
       );
   };
