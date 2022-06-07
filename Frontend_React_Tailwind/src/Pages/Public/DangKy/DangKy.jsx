3401dbc04ac537797193e7d6fda0a8596a19d970
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-06-07 18:29:20 +0700

    validate form đăng ký

--- Frontend_React_Tailwind/src/Pages/Public/DangKy/DangKy.jsx
+++ Frontend_React_Tailwind/src/Pages/Public/DangKy/DangKy.jsx
@@ -32,66 +32,105 @@ export default function DangKy() {
         <input
           className={inputStyle}
           {...register('hoTen', {
-            required: true
+            required: true,
+            minLength: 10,
+            maxLength: 50
           })}
         />
+        {errors?.hoTen?.type === 'required' && <p className={errorStyle}>Tên khách hàng bắt buộc nhập</p>}
+        {errors?.hoTen?.type === 'minLength' && <p className={errorStyle}>Tên khách hàng tối thiểu 10 kí tự</p>}
+        {errors?.hoTen?.type === 'maxLength' && <p className={errorStyle}>Tên khách hàng tối đa 50 kí tự</p>}
         <br />
         <label className={labelStyle}>Dia Chi</label>
         <input
           className={inputStyle}
           {...register('diaChi', {
-            required: true
+            required: true,
+            minLength: 30,
+            maxLength: 50
           })}
-        />{' '}
+        />
+        {errors?.diaChi?.type === 'required' && <p className={errorStyle}>Địa chỉ bắt buộc nhập</p>}
+        {errors?.diaChi?.type === 'minLength' && <p className={errorStyle}>Địa chỉ tối thiểu 30 kí tự</p>}
+        {errors?.diaChi?.type === 'maxLength' && <p className={errorStyle}>Địa chỉ tối đa 50 kí tự</p>}
         <br />
         <label className={labelStyle}>So Dien Thoai</label>
         <input
           className={inputStyle}
           {...register('soDienThoai', {
-            required: true
+            required: true,
+            minLength: 10,
+            maxLength: 10,
+            valueAsNumber: true
           })}
-        />{' '}
+        />
+        {errors?.soDienThoai?.type === 'required' && <p className={errorStyle}>Số điện thoại bắt buộc nhập</p>}
+        {errors?.soDienThoai?.type === 'maxLength' && <p className={errorStyle}>Số điện thoại phải 10 kí số</p>}
+        {errors?.soDienThoai?.type === 'valueAsNumber' && <p className={errorStyle}>Số điện thoại bắt buộc kiểu số</p>}
         <br />
         <label className={labelStyle}>Gioi Tinh</label>
-        <input
+        <select
           className={inputStyle}
           {...register('gioiTinh', {
             required: true
-          })}
-        />{' '}
+          })}>
+          <option value='1' selected>
+            Nam
+          </option>
+          <option value='0'>Nữ</option>
+        </select>
+        {errors?.gioiTinh?.type === 'required' && <p className={errorStyle}>Giới tính bắt buộc chọn</p>}
         <br />
         <label className={labelStyle}>Username</label>
         <input
           className={inputStyle}
           {...register('username', {
-            required: true
+            required: true,
+            minLength: 10,
+            maxLength: 20
           })}
-        />{' '}
+        />
+        {errors?.username?.type === 'required' && <p className={errorStyle}>Username bắt buộc nhập</p>}
+        {errors?.username?.type === 'minLength' && <p className={errorStyle}>Username tối thiểu 10 kí tự</p>}
+        {errors?.username?.type === 'maxLength' && <p className={errorStyle}>Username tối đa 20 kí tự</p>}
         <br />
         <label className={labelStyle}>Password</label>
         <input
           className={inputStyle}
-          {...register('retype-password', {
-            required: true
+          {...register('password', {
+            required: true,
+            minLength: 10,
+            maxLength: 20
           })}
-        />{' '}
+        />
+        {errors?.password?.type === 'required' && <p className={errorStyle}>Password bắt buộc nhập</p>}
+        {errors?.password?.type === 'minLength' && <p className={errorStyle}>Password tối thiểu 10 kí tự</p>}
+        {errors?.password?.type === 'maxLength' && <p className={errorStyle}>Password tối đa 20 kí tự</p>}
         <br />
         <label className={labelStyle}>Nhap Lai Password</label>
         <input
           className={inputStyle}
-          {...register('password', {
-            required: true
+          {...register('retype_password', {
+            required: true,
+            minLength: 10,
+            maxLength: 20
           })}
-        />{' '}
+        />
+        {errors?.retype_password?.type === 'required' && <p className={errorStyle}>Password bắt buộc nhập</p>}
+        {errors?.retype_password?.type === 'minLength' && <p className={errorStyle}>Password tối thiểu 10 kí tự</p>}
+        {errors?.retype_password?.type === 'maxLength' && <p className={errorStyle}>Password tối đa 20 kí tự</p>}
         <br />
-        <lable>Email</lable>{' '}
+        <lable>Email</lable>
         <input
           className={inputStyle}
           {...register('email', {
-            required: true
+            required: true,
+            pattern: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
           })}
         />
-        <input type="submit" value="Dang Ky"/>
+        {errors?.email?.type === 'required' && <p className={errorStyle}>Email bắt buộc nhập</p>}
+        {errors?.email?.type === 'pattern' && <p className={errorStyle}>Email không đúng định dạng</p>}   
+        <input type='submit' value='Dang Ky' />
       </form>
     </div>
   );
