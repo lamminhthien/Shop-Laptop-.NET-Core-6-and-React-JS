f577c947102d6188f981a0ea5a50f4fdb3d2c6db
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-06-07 07:02:21 +0700

    restore html tag

--- Frontend_React_Tailwind/src/Components/Public/ProductList.jsx
+++ Frontend_React_Tailwind/src/Components/Public/ProductList.jsx
@@ -53,40 +53,39 @@ export default function ProductList() {
     if (pageState === -1) return <p className='text-red-500 text-2xl'>Trang bạn yêu cầu có lỗi</p>;
     if (pageState === 1)
       return (
-        // <div className='container mt-5'>
-        //   <div className='flex flex-wrap justify-start'>
-        //     {listProduct.data.ketqua.map(item => (
-        //       <a
-        //         href={item.maSanPham}
-        //         className='h-fit
-        //             bg-white shadow-lg
-        //             rounded-md border-black border-0 w-60 flex
-        //             flex-wrap justify-center mx-4 my-4
-        //             '>
-        //         <div
-        //           className='
-        //         max-h-40 bg-center px-3 py-3
-        //         '>
-        //           <img src='https://vatvostudio.vn/wp-content/uploads/2022/04/iPhone-12-hang-Nhat-ma-ja-ve-Viet-Nam-2.jpg' />
-        //         </div>
-        //         <p
-        //           className='
-        //             font-bold text-sm text-black w-full text-center my-3
-        //           '>
-        //           {item.tenSanPham}
-        //         </p>
-        //         <div
-        //           className='
-        //         bg-orange-500 w-fit px-4 text-white
-        //          border-orange-500 rounded-2xl py-3
-        //         '>
-        //           {item.giaNiemYet}đ
-        //         </div>
-        //       </a>
-        //     ))}
-        //   </div>
-        // </div>
-        <>{JSON.stringify(listProduct.data.ketqua)}</>
+        <div className='container mt-5'>
+          <div className='flex flex-wrap justify-start'>
+            {listProduct.data.ketqua.map(item => (
+              <a
+                href={item.maSanPham}
+                className='h-fit
+                    bg-white shadow-lg
+                    rounded-md border-black border-0 w-60 flex
+                    flex-wrap justify-center mx-4 my-4
+                    '>
+                <div
+                  className='
+                max-h-40 bg-center px-3 py-3
+                '>
+                  <img src='https://vatvostudio.vn/wp-content/uploads/2022/04/iPhone-12-hang-Nhat-ma-ja-ve-Viet-Nam-2.jpg' />
+                </div>
+                <p
+                  className='
+                    font-bold text-sm text-black w-full text-center my-3
+                  '>
+                  {item.tenSanPham}
+                </p>
+                <div
+                  className='
+                bg-orange-500 w-fit px-4 text-white
+                 border-orange-500 rounded-2xl py-3
+                '>
+                  {item.giaNiemYet}đ
+                </div>
+              </a>
+            ))}
+          </div>
+        </div>
       );
   };
   return <>{pageRender()}</>;
