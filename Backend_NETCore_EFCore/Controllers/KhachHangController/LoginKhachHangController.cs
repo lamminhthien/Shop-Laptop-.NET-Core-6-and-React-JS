ff9f89dd053be973b93e1f0966f08e16ca32248f
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-06-08 16:29:42 +0700

    Update LoginKhachHangController.cs

--- Backend_NETCore_EFCore/Controllers/KhachHangController/LoginKhachHangController.cs
+++ Backend_NETCore_EFCore/Controllers/KhachHangController/LoginKhachHangController.cs
@@ -17,72 +17,90 @@ using System.Text;
 
 namespace ShopLaptop_EFCore.Controllers.KhachHangController
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class LoginKhachHangController : ControllerBase
+  [Route("api/[controller]")]
+  [ApiController]
+  public class LoginKhachHangController : ControllerBase
+  {
+    // Đọc cấu hình từ appsetting.json
+    private IConfiguration _config;
+    // Database context
+    private readonly shop_laptopContext _context;
+    // Constructor
+    public LoginKhachHangController(IConfiguration config, shop_laptopContext context)
     {
-        // Đọc cấu hình từ appsetting.json
-        private IConfiguration _config;
-        // Database context
-        private readonly shop_laptopContext _context;
-        // Constructor
-        public LoginKhachHangController(IConfiguration config, shop_laptopContext context)
+      _config = config;
+      _context = context;
+    }
+
+    // Route đăng nhập và lấy jwt token
+    [AllowAnonymous]
+    [HttpPost]
+    public IActionResult LoginKhachHang()
+    {
+      var username = Request.Form["username"][0];
+      var password = Request.Form["password"][0];
+      // Kiểm tra username và password có tồn tại trong database không?
+      var currentUser = _context.KhachHangs.FirstOrDefault(o => o.Username == username && o.Password == password);
+
+      // Nếu tồn tại tài khoản, trả về JWT Token để React lưu vào LocalStorage
+      if (currentUser != null)
+      {
+        var securityKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var claims = new[]
         {
-            _config = config;
-            _context = context;
-        }
-
-        // Route đăng nhập và lấy jwt token
-        [AllowAnonymous]
-        [HttpPost]
-        public IActionResult LoginKhachHang()
-        {
-            var username = Request.Form["username"][0];
-            var password = Request.Form["password"][0];
-            // Kiểm tra username và password có tồn tại trong database không?
-            var currentUser = _context.KhachHangs.FirstOrDefault(o => o.Username == username && o.Password == password);
-
-            // Nếu tồn tại tài khoản, trả về JWT Token để React lưu vào LocalStorage
-            if (currentUser != null)
-            {
-                var securityKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
-                var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
-
-                var claims = new[]
-                {
                     new Claim(ClaimTypes.NameIdentifier, currentUser.Username),
                     new Claim(ClaimTypes.MobilePhone, currentUser.SoDienThoai),
                     new Claim(ClaimTypes.Name, currentUser.HoTen),
                     new Claim(ClaimTypes.Role, "Khách Hàng")
                  };
 
-                var token = new JwtSecurityToken(_config["Jwt:Issuer"],
-                  _config["Jwt:Audience"],
-                  claims,
-                  expires:DateTime.Now.AddDays(1),
-                  signingCredentials: credentials);
-                // Mã hóa thành chuỗi token và trả về status 200 kèm token để React lưu vào LocalStorage
-                var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
-                return Ok(tokenString);
-            }
-            else
-            {
-                return NotFound("Tài khoản khách hàng không hợp lệ");
-            }
-        }
-        // Lấy thông tin tài khoản khách hàng từ JWT Token
-        [Authorize(Roles="Khách Hàng")]
-        [HttpPost("GetKhachHangName")]
-        public IActionResult GetKhachHangName()
-        {
-            // Đọc các Claims từ Header của Request
-            var identity = HttpContext.User.Identity as ClaimsIdentity;
-            // Nếu tồn tại, lấy tên của khách hàng
-            if (identity != null)
-            {
-                return Ok(identity.Claims.FirstOrDefault(o=>o.Type == ClaimTypes.NameIdentifier)?.Value);
-            }
-            return NotFound("Không tìm thấy gì");
-        }
+        var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+          _config["Jwt:Audience"],
+          claims,
+          expires: DateTime.Now.AddDays(1),
+          signingCredentials: credentials);
+        // Mã hóa thành chuỗi token và trả về status 200 kèm token để React lưu vào LocalStorage
+        var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+        return Ok(tokenString);
+      }
+      else
+      {
+        return NotFound("Tài khoản khách hàng không hợp lệ");
+      }
     }
+    // Lấy thông tin tài khoản khách hàng từ JWT Token
+    [Authorize(Roles = "Khách Hàng")]
+    [HttpPost("GetKhachHangName")]
+    public IActionResult GetKhachHangName()
+    {
+      // Đọc các Claims từ Header của Request
+      var identity = HttpContext.User.Identity as ClaimsIdentity;
+      // Nếu tồn tại, lấy tên của khách hàng
+      if (identity != null)
+      {
+        return Ok(identity.Claims.FirstOrDefault(o => o.Type == ClaimTypes.Name)?.Value);
+      }
+      return NotFound("Không tìm thấy gì");
+    }
+
+    // Truy xuất mã khách hàng từ username
+    [Authorize(Roles = "Khách Hàng")]
+    [HttpPost("GetMaKhachHang")]
+    public IActionResult GetMaKhachHang()
+    {
+      // Đọc các Claims từ Header của Request
+      var identity = HttpContext.User.Identity as ClaimsIdentity;
+      // Nếu tồn tại, lấy tên của khách hàng
+      if (identity != null)
+      {
+        var tenKhachHang = identity.Claims.FirstOrDefault(o => o.Type == ClaimTypes.Name)?.Value;
+        var idKhachHang = (from a in _context.KhachHangs 
+        where a.HoTen == tenKhachHang select a.MaKhachHang).FirstOrDefault();
+        return Ok(idKhachHang);
+      }
+      return NotFound("Không tìm thấy gì");
+    }
+  }
 }
