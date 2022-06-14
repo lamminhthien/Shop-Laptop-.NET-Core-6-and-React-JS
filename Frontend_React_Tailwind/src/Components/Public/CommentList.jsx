6a570c096601c96d91597dab57b0ce39a873be6a
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-06-14 18:32:09 +0700

    Update CommentList.jsx

--- Frontend_React_Tailwind/src/Components/Public/CommentList.jsx
+++ Frontend_React_Tailwind/src/Components/Public/CommentList.jsx
@@ -1,11 +1,13 @@
 import { useState,useEffect } from 'react';
 import axios from 'axios';
+import { useParams } from "react-router-dom";
 
 export default function CommentList() {
   const [listBinhLuan,setListBinhLuan] = useState([])
   const [state,setState] = useState(false)
+  let { id } = useParams();
   useEffect(()=>{
-    axios.get("https://localhost:7216/api/TrangChiTietSanPham/ListBinhLuanSanPham?id=1").then((res)=>{
+    axios.get(`https://localhost:7216/api/TrangChiTietSanPham/ListBinhLuanSanPham?id=${id}`).then((res)=>{
       console.log("%cThis is a green text", "color:green");
       setListBinhLuan(res.data);
       setState(true)
