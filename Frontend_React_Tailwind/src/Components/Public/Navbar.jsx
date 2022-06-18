70d6917b3e3be968894e23128b42bb69318d351f
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-06-18 19:14:49 +0700

    add button tình trạng đơn hàng

--- Frontend_React_Tailwind/src/Components/Public/Navbar.jsx
+++ Frontend_React_Tailwind/src/Components/Public/Navbar.jsx
@@ -47,6 +47,13 @@ export default function Navbar() {
             </a>
           </li>
           <li>
+              <a
+                href='/khach-hang/don-hang'
+                className='block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 '>
+                Tình trạng đơn hàng
+              </a>
+            </li>
+          <li>
             <p
               className='block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 '>
               Xin chào {userName}
