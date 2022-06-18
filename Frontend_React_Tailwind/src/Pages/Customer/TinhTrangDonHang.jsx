f2f504ae11eeeac917ad2ef1b356d9711c96d084
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-06-18 20:46:45 +0700

    init layout tình trạng đơn hàng

--- Frontend_React_Tailwind/src/Pages/Customer/TinhTrangDonHang.jsx
+++ Frontend_React_Tailwind/src/Pages/Customer/TinhTrangDonHang.jsx
@@ -12,31 +12,60 @@ export default function TinhTrangDonHang() {
             <div className=' title'>
               <p className='text-3xl font-medium leading-6'>Tình trạng đơn hàng</p>
             </div>
-            <div className='list_item'>
-              <div className='inner grid grid-cols-2 px-8 py-4'>
-                <div className='flex justify-start items-center name-and-image-product  pl-6 gap-x-6'>
-                  <div className='w-[100px] '>
-                    <img src='https://localhost:7216/Resources/Images/SanPham/SP67-1.jpeg' alt='Dell AlienWare m15 ' />
+            <div className='list_don_hang'>
+              <div className='inner px-8 py-4'>
+                <div className="don-hang space-y-10">
+                  <div className="don-hang-1">
+                    <h2>Đơn hàng 1</h2>
+                    <div className="list_item_in_don_hang  space-y-4 pl-5">
+                      <div className="item flex justify-between space-x-9">
+                        <h2>Ảnh</h2>
+                        <h2>Dell Alien Ware</h2>
+                        <p>16</p>
+                        <p>5000000</p>
+                        <p>Đang chờ duyệt đơn</p>
+                      </div>
+                      <div className="item flex space-x-9 justify-between">
+                        <h2>Ảnh</h2>
+                        <h2>Dell Alien Ware</h2>
+                        <p>16</p>
+                        <p>5000000</p>
+                        <p>Đang chờ duyệt đơn</p>
+                      </div>
+                      <div className="item flex space-x-9 justify-between">
+                        <h2>Ảnh</h2>
+                        <h2>Dell Alien Ware</h2>
+                        <p>16</p>
+                        <p>5000000</p>
+                        <p>Đang chờ duyệt đơn</p>
+                      </div>
+                    </div>
                   </div>
-                  <div className='inner'>
-                    <h2 className='text-lg font-medium leading-5'>Dell AlienWare m15 </h2>
-                  </div>
-                </div>
-                <div className='flex justify-end items-center function-control-each-items space-x-12'>
-                  <div className='control-quantity space-x-4'>
-                    <input
-                      type='number'
-                      className='w-[70px] border-2 border-gray-400 rounded-md'
-                      min={1}
-                      max={4}
-                      defaultValue={1}
-                    />
-                  </div>
-                  <div className='total-money'>
-                    <p className='text-lg font-bold leading-5'>242000</p>
-                  </div>
-                  <div className='delete'>
-                    <button className='text-lg font-bold text-red-500 leading-5'>Delete</button>
+                  <div className="don-hang-2">
+                    <h2>Đơn hàng 2</h2>
+                    <div className="list_item_in_don_hang  space-y-4 pl-5">
+                      <div className="item flex space-x-9 justify-between">
+                        <h2>Ảnh</h2>
+                        <h2>Dell Alien Ware</h2>
+                        <p>16</p>
+                        <p>5000000</p>
+                        <p>Đang chờ duyệt đơn</p>
+                      </div>
+                      <div className="item flex space-x-9 justify-between">
+                        <h2>Ảnh</h2>
+                        <h2>Dell Alien Ware</h2>
+                        <p>16</p>
+                        <p>5000000</p>
+                        <p>Đang chờ duyệt đơn</p>
+                      </div>
+                      <div className="item flex space-x-9 justify-between">
+                        <h2>Ảnh</h2>
+                        <h2>Dell Alien Ware</h2>
+                        <p>16</p>
+                        <p>5000000</p>
+                        <p>Đang chờ duyệt đơn</p>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
