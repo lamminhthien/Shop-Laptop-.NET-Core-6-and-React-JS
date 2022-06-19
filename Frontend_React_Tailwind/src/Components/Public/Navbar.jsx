3b1156797ad31332deb4d7ad25d9d430709fa151
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-06-19 09:17:34 +0700

    add logo mới

--- Frontend_React_Tailwind/src/Components/Public/Navbar.jsx
+++ Frontend_React_Tailwind/src/Components/Public/Navbar.jsx
@@ -72,7 +72,7 @@ export default function Navbar() {
     <nav className='bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800'>
       <div className='container flex flex-wrap justify-between items-center mx-auto'>
         <a href='/' className='flex items-center'>
-          <img src='https://flowbite.com/docs/images/logo.svg' className='mr-3 h-6 sm:h-9' alt='Flowbite Logo' />
+          <img src='/logo.svg' className='mr-3 h-6 sm:h-9' alt='Flowbite Logo' />
           <span className='self-center text-xl font-semibold whitespace-nowrap dark:text-white'>Đức Thịnh Laptop</span>
         </a>
         <button
