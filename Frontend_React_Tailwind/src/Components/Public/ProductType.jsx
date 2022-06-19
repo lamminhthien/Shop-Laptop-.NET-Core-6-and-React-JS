5694850e4f326e938c7c8d7547ba48400ecf036d
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-06-19 21:11:44 +0700

    reduce size danh mục sản phẩm

--- Frontend_React_Tailwind/src/Components/Public/ProductType.jsx
+++ Frontend_React_Tailwind/src/Components/Public/ProductType.jsx
@@ -22,20 +22,17 @@ export default function ProductType() {
 if (state.done)
   return (
   
-    <div>
+    <div className='danh-muc-san-pham'>
         <h1 className="text-center text-fuchsia-500 text-3xl font-semibold mt-5 mb-5">Danh mục sản phẩm</h1>
       <div class="flex flex-wrap place-content-center gap-4 overflow-hidden">
-      
          {state.data !== null ? state.data.map((item) =>
           <a href={`?category=${item.maLoaiSanPham}`}>
-            <div className="w-60 p-2 bg-white rounded-xl transform transition-all 
-            border-indigo-600 border-2 flex flex-wrap justify-center
-            hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl">
+            <div className="flex w-48 h-40 transform flex-wrap justify-center rounded-xl border-2 border-indigo-600 bg-white p-2 shadow-lg transition-all duration-300 hover:-translate-y-2 hover:shadow-2xl">
               {/* Image */}
-              <img className="h-40 object-cover rounded-xl h-40 object-cover rounded-xl" src={item.anhLoaiSanPham} alt />
+              <img className="h-28" src={item.anhLoaiSanPham} alt />
               <div className="p-2">
                 {/* Heading */}
-                <h2 className="font-bold text-lg mb-2 ">{item.tenLoaiSanPham}</h2>
+                <h2 className="mb-2 text-lg font-bold">{item.tenLoaiSanPham}</h2>
               </div>
               {/* CTA */}
             </div>
