4e0447e4e65468ce668a96c2c6c0a071f69f42d6
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-06-19 20:09:28 +0700

    resize thương hiệu on homepage

--- Frontend_React_Tailwind/src/Components/Public/BrandList.jsx
+++ Frontend_React_Tailwind/src/Components/Public/BrandList.jsx
@@ -21,24 +21,18 @@ export default function BrandList() {
         return (
             <div>
                  <h1 className="text-center text-fuchsia-500 my-5  text-3xl font-semibold">Thương hiệu</h1>
-                <div className="">
-                    <a href="#" class="flex flex-wrap place-content-center gap-5">
+                <div className="brand-list">
+                    <div href="#" class="flex flex-wrap place-content-center gap-5">
                         {state.data !== null ? state.data.map((item) =>
                             <a href={`?brand=${item.maHangSanXuat}`}>
-                                <div className="h-60 flex flex-col flex-wrap  p-2 bg-white rounded-xl border-0  border-indigo-600 border-2
-                                transform transition-all hover:-translate-y-2 flex flex-wrap justify-center
-                                duration-300 shadow-lg hover:shadow-2xl">
+                                <div className="flex flex h-[100px] w-[100px] transform flex-col flex-wrap flex-wrap justify-center rounded-xl border-0 border-2 border-indigo-600 bg-white p-2 shadow-lg transition-all duration-300 hover:-translate-y-2 hover:shadow-2xl">
                                     {/* Image */}
-                                    <img className="h-40 object-fill rounded-xl " src={item.logo} alt={item.tenHangSanXuat} />
-                                    <div className="p-2">
-                                        {/* Heading */}
-                                        <h2 className="font-bold text-lg mb-2 text-center">{item.tenHangSanXuat}</h2>
-                                    </div>
+                                    <img className="w-36 rounded-xl object-cover" src={item.logo} alt={item.tenHangSanXuat} />
                                     {/* CTA */}
                                 </div>
                             </a>
                         ) : <p>Đang load data</p>}
-                    </a>
+                    </div>
                 </div>
             </div>
         )
