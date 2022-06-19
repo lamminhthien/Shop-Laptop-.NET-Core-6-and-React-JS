8f596aa502bf2f183bd8d3ab269e2830b1ad2cfc
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-06-19 07:20:08 +0700

    styling tinhTrangDonHang jsx

--- Frontend_React_Tailwind/src/Pages/Customer/TinhTrangDonHang.jsx
+++ Frontend_React_Tailwind/src/Pages/Customer/TinhTrangDonHang.jsx
@@ -43,20 +43,20 @@ export default function TinhTrangDonHang() {
                     <div className='inner px-8 py-4'>
                       <div className='don-hang space-y-10'>
                         {objectDonHang.groupChiTietHoaDon.map(item => (
-                          <div className='don-hang-1'>
+                          <div className='don-hang-1 bg-slate-100'>
                             <div className='flex justify-between'>
-                              <h2>
-                                Mã đơn hàng {item.soHoaDon} - {item.tinhTrang}
+                              <h2 className='text-2xl'>
+                                Mã đơn hàng {item.soHoaDon} - <b className={item.tinhTrang==='Đang chờ duyệt' ? "text-red-500" : "text-green-500"}>{item.tinhTrang}</b>
                               </h2>
-                              <p>Tổng tiền: {item.tongTien}</p>
+                              <p className='text-lg font-bold'>Tổng tiền: {item.tongTien}</p>
                             </div>
-                            <div className='list_item_in_don_hang  space-y-4 pl-5'>
+                            <div className='list_item_in_don_hang  space-y-4 pl-10 border-4 border-b-emerald-400 border-t-emerald-400'>
                               {item.chiTietHoaDonList.map(item_inner => (
-                                <div className='item flex space-x-9 justify-between'>
-                                  <img className='w-[200px]' src={item_inner.anhSanPham} alt={item_inner.tenSanPham} />
-                                  <h2>{item_inner.tenSanPham}</h2>
-                                  <p>Số lượng: {item_inner.soLuong}</p>
-                                  <p>Đơn giá: {item_inner.donGia}</p>
+                                <div className='text-xl item flex space-x-9 justify-between'>
+                                  <img className='h-[200px] self-center' src={item_inner.anhSanPham} alt={item_inner.tenSanPham} />
+                                  <h2 className=' text-gray-700 self-center'>{item_inner.tenSanPham}</h2>
+                                  <p className='font-bold text-red-500 self-center'>Số lượng: {item_inner.soLuong}</p>
+                                  <p className='self-center '>Đơn giá: {item_inner.donGia}</p>
                                 </div>
                               ))}
                             </div>
