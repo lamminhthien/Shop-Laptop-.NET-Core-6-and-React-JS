7bf059b72619d1b157f865e3587cf3b277880091
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-06-19 19:16:29 +0700

    hidden form bình luận sản phẩm when don't login

--- Frontend_React_Tailwind/src/Components/Public/ProductDetail.jsx
+++ Frontend_React_Tailwind/src/Components/Public/ProductDetail.jsx
@@ -1,19 +1,19 @@
-import { useParams } from 'react-router-dom';
+import {useParams} from 'react-router-dom';
 import TrangChiTietSanPhamApi from '../../Api/Public/TrangChiTietSanPhamApi';
-import { useState, useEffect } from 'react';
-import axios  from 'axios';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 import CommentForm from './CommentForm';
 
 export default function ProductDetail() {
-  let { id } = useParams();
+  let {id} = useParams();
   const [state, setState] = useState({});
   const [isAdmin, setIsAdmin] = useState(false);
+  
   useEffect(() => {
     //Check NhanVien to block button thêm vào giỏ hàng
     axios.get('https://localhost:7216/api/LoginNhanVien/validateToken', configJWT).then(res => {
       setIsAdmin(true);
-    });
-
+    })
     // Get chi tiet san pham
     TrangChiTietSanPhamApi.getChiTietSanPham(id)
       .then(res => {
@@ -38,82 +38,89 @@ export default function ProductDetail() {
     }
   };
 
-  const themGioHang = (maSanPham) => {
-    const formData = new FormData()
-      formData.append("maSanPham",maSanPham)
-      formData.append("soLuong",1)
-    axios.post('https://localhost:7216/api/GioHang/ThemGioHang',formData,configJWT)
-    .then(res => {
-      alert("Đã thêm vào giỏ hàng thành công")
-    }).catch(err => {
-      alert("Sản phẩm này đã tồn tại trong giỏ hàng");
-    })
+  const themGioHang = maSanPham => {
+    const formData = new FormData();
+    formData.append('maSanPham', maSanPham);
+    formData.append('soLuong', 1);
+    axios
+      .post('https://localhost:7216/api/GioHang/ThemGioHang', formData, configJWT)
+      .then(res => {
+        alert('Đã thêm vào giỏ hàng thành công');
+      })
+      .catch(err => {
+        alert('Sản phẩm này đã tồn tại trong giỏ hàng');
+      });
   };
 
   if (state.done) {
     return (
       <>
-      <section className='text-gray-600 body-font overflow-hidden'>
-        <div className='container px-5 py-24 mx-auto'>
-          <div className='flex flex-row'>
-            <div className='lg:basis-2/4'>
-              <img
-                alt='ecommerce'
-                className='m-auto h-3/5  w-96 h-96 object-cover object-center rounded'
-                src={state.imageSlide}
-              />
-              <div className='flex flex-row mt-3  justify-center space-x-4 font-mono text-white text-sm font-bold leading-6'>
-                {state.image !== null ? (
-                  state.image.map(item => (
-                    <img
-                      alt='ecommerce'
-                      className='w-48 h-48 object-cover object-center rounded'
-                      src={item}
-                      onClick={() => setState({ ...state, imageSlide: item })}
-                    />
-                  ))
-                ) : (
-                  <p>Đang load ảnh thu nhỏ</p>
-                )}
+        <section className='text-gray-600 body-font overflow-hidden'>
+          <div className='container px-5 py-24 mx-auto'>
+            <div className='flex flex-row'>
+              <div className='lg:basis-2/4'>
+                <img
+                  alt='ecommerce'
+                  className='m-auto h-3/5  w-96 h-96 object-cover object-center rounded'
+                  src={state.imageSlide}
+                />
+                <div className='flex flex-row mt-3  justify-center space-x-4 font-mono text-white text-sm font-bold leading-6'>
+                  {state.image !== null ? (
+                    state.image.map(item => (
+                      <img
+                        alt='ecommerce'
+                        className='w-48 h-48 object-cover object-center rounded'
+                        src={item}
+                        onClick={() => setState({...state, imageSlide: item})}
+                      />
+                    ))
+                  ) : (
+                    <p>Đang load ảnh thu nhỏ</p>
+                  )}
+                </div>
               </div>
-            </div>
-            <div className='lg:basis-2/4 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0'>
-              <h2 className='text-sm title-font text-gray-500 tracking-widest'>{state.data.hangSanXuat}</h2>
-              <h1 className='text-gray-900 text-3xl title-font font-medium mb-1'>{state.data.tenSanPham}</h1>
-              <div className='flex mb-4'>
-                <span className='flex items-center'>
-                  <span className='text-indigo-500 ml-3'>{state.data.loaiSanPham}</span>
-                </span>
-              </div>
-              <div className='leading-relaxed'>
-                <ul class='list-disc list-inside text-slate-900 dark:text-slate-200'>
-                  <li>CPU: {state.data.cpu}</li>
-                  <li>Card đồ họa: {state.data.cardDoHoa}</li>
-                  <li>Độ phân giải: {state.data.doPhanGiai}</li>
-                  <li>Ổ cứng: {state.data.oCung}</li>
-                  <li>Hệ điều hành: {state.data.heDieuHanh}</li>
-                  <li>Kích Thước: {state.data.kichThuoc}</li>
-                  <li>Màn hình: {state.data.manHinh}</li>
-                  <li>Trọng lượng: {state.data.trongLuong}</li>
-                  <li>RAM: {state.data.ram}</li>
-                  <li>Mô tả thêm: {state.data.moTaThem}</li>
-                </ul>
-              </div>
-              <div className='flex'>
-                <span className='title-font font-medium text-2xl text-gray-900'>Giá: {state.data.giaNiemYet} VND</span>
-                {isAdmin ? "" :    <button
-                  onClick={() => themGioHang(state.data.maSanPham)}
-                  className='flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded'>
-                  Thêm vào giỏ hàng
-                </button>}
+              <div className='lg:basis-2/4 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0'>
+                <h2 className='text-sm title-font text-gray-500 tracking-widest'>{state.data.hangSanXuat}</h2>
+                <h1 className='text-gray-900 text-3xl title-font font-medium mb-1'>{state.data.tenSanPham}</h1>
+                <div className='flex mb-4'>
+                  <span className='flex items-center'>
+                    <span className='text-indigo-500 ml-3'>{state.data.loaiSanPham}</span>
+                  </span>
+                </div>
+                <div className='leading-relaxed'>
+                  <ul class='list-disc list-inside text-slate-900 dark:text-slate-200'>
+                    <li>CPU: {state.data.cpu}</li>
+                    <li>Card đồ họa: {state.data.cardDoHoa}</li>
+                    <li>Độ phân giải: {state.data.doPhanGiai}</li>
+                    <li>Ổ cứng: {state.data.oCung}</li>
+                    <li>Hệ điều hành: {state.data.heDieuHanh}</li>
+                    <li>Kích Thước: {state.data.kichThuoc}</li>
+                    <li>Màn hình: {state.data.manHinh}</li>
+                    <li>Trọng lượng: {state.data.trongLuong}</li>
+                    <li>RAM: {state.data.ram}</li>
+                    <li>Mô tả thêm: {state.data.moTaThem}</li>
+                  </ul>
+                </div>
+                <div className='flex'>
+                  <span className='title-font font-medium text-2xl text-gray-900'>
+                    Giá: {state.data.giaNiemYet} VND
+                  </span>
+                  {isAdmin ? (
+                    ''
+                  ) : (
+                    <button
+                      onClick={() => themGioHang(state.data.maSanPham)}
+                      className='flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded'>
+                      Thêm vào giỏ hàng
+                    </button>
+                  )}
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
-      <CommentForm maSanPham={id}/>
+        </section>
+        <CommentForm maSanPham={id} />
       </>
-     
     );
   }
 }
