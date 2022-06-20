6d77521d8c75ce2a8aff173950d4774adbf3937a
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-06-20 16:48:21 +0700

    add button to filter in front edn

--- Frontend_React_Tailwind/src/Pages/Admin/DonHang/ListDonHang.jsx
+++ Frontend_React_Tailwind/src/Pages/Admin/DonHang/ListDonHang.jsx
@@ -36,26 +36,31 @@ export default function ListDonHang() {
               <div className='inner border-2 border-white rounded-lg'>
                 <div className='items-cart relative w-full   lg:p-6 bg-white space-y-10 '>
                   <div className=' title'>
-                    <p className='text-3xl font-medium leading-6'>Danh sách đơn hàng</p>
+                    <p className='text-3xl font-medium leading-6 py-2'>Danh sách đơn hàng</p>
+                    <div className='flex gap-x-3 filter-don-hang'>
+                      <button className='bg-red-500 border-2 rounded-xl p-1 text-white'>Chưa duyệt</button>
+                      <button className='bg-yellow-500 border-2 rounded-xl p-1 text-white'>Đang vận chuyển</button>
+                      <button className='bg-green-500 border-2 rounded-xl p-1 text-white'>Hoàn thành</button>
+                    </div>
                   </div>
                   <div class='list_don_hang'>
-                  {listHoaDon.map(item => (
-                    <div class='inner px-8 py-4'>
-                      <div class='don-hang space-y-10'>
-                        <div class='don-hang-1 bg-slate-100'>
-                          <div class=''>
-                            <h2 class='text-2xl'>
-                              Mã đơn hàng 16 - <b class='text-red-500'>{item.tinhTrang}</b>
-                            </h2>
-                            <p class='text-lg font-bold'>Tên khách hàng: {item.hoTen}</p>
-                            <p class='text-lg font-bold'>Thời gian : {item.thoiGian.slice(0,10)}</p>
-                            <p class='text-lg font-bold'>Số lượng sản phẩm: {item.soLuongSanPham}</p>
-                            <p class='text-lg font-bold'>Tổng tiền: {item.tongTien}</p>
+                    {listHoaDon.map(item => (
+                      <div class='inner px-8 py-4'>
+                        <div class='don-hang space-y-10'>
+                          <div class='don-hang-1 bg-slate-100'>
+                            <div class=''>
+                              <h2 class='text-2xl'>
+                                Mã đơn hàng 16 - <b class='text-red-500'>{item.tinhTrang}</b>
+                              </h2>
+                              <p class='text-lg font-bold'>Tên khách hàng: {item.hoTen}</p>
+                              <p class='text-lg font-bold'>Thời gian : {item.thoiGian.slice(0, 10)}</p>
+                              <p class='text-lg font-bold'>Số lượng sản phẩm: {item.soLuongSanPham}</p>
+                              <p class='text-lg font-bold'>Tổng tiền: {item.tongTien}</p>
+                            </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  ))}
+                    ))}
                   </div>
                 </div>
               </div>
@@ -68,13 +73,13 @@ export default function ListDonHang() {
   if (state === false) {
     return (
       <div>
-      <div className='flex'>
-        <Sidebar />
-        <div className='h-screen flex-1 p-7'>
-         <h2 className='text-red-500 font-semibold leading-7'>Chưa có hóa đơn nào</h2>
+        <div className='flex'>
+          <Sidebar />
+          <div className='h-screen flex-1 p-7'>
+            <h2 className='text-red-500 font-semibold leading-7'>Chưa có hóa đơn nào</h2>
+          </div>
         </div>
       </div>
-    </div>
-    )
+    );
   }
 }
