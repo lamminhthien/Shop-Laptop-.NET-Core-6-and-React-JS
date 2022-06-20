392d2c8d59b2e2018e8a32d773b273d8b4938af3
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-06-20 22:28:37 +0700

    Update QuanLyDonHangController.cs

--- Backend_NETCore_EFCore/Controllers/NhanVienController/QuanLyDonHangController.cs
+++ Backend_NETCore_EFCore/Controllers/NhanVienController/QuanLyDonHangController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using System.Net.Http.Headers;
 using System.IO;
+using System.Security.Claims;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -75,42 +76,49 @@ namespace ShopLaptop_EFCore.Controllers.NhanVienController
     }
 
     [HttpPost("CapNhatTrangThaiDon")]
-    public async Task<ActionResult<List<dynamic>>> CapNhatTrangThaiDon(int maHoaDon = -1, int maNhanVien = -1, int trangThaiDon = -2)
+    public async Task<ActionResult<List<dynamic>>> CapNhatTrangThaiDon(int maHoaDon = -1, int trangThaiDon = -2)
     {
-      var hoaDon = (from a in _context.HoaDons
-                    where a.MaHoaDon == maHoaDon
-                    select a).FirstOrDefault();
-      if (hoaDon == null) return BadRequest("Không tìm thấy hóa đơn ");
-      var nhanVien = (from a in _context.NhanViens
-                      where a.MaNhanVien == maNhanVien
-                      select a).FirstOrDefault();
-      if (nhanVien == null) return BadRequest("Không tìm thấy nhân viên ");
-      if (trangThaiDon > 1 && trangThaiDon < -1) return BadRequest("Trạng thái đơn hàng cần cập nhật không hợp lệ");
-      hoaDon.NgayChotDon = System.DateTime.Now;
-      hoaDon.TinhTrangGiaoHang = trangThaiDon;
-      _context.Entry(hoaDon).State = EntityState.Modified;
-      try
-      {
-        _context.SaveChanges();
+      var identity = HttpContext.User.Identity as ClaimsIdentity;
+      var userNameNhanVien = identity.Claims.FirstOrDefault(o => o.Type == ClaimTypes.NameIdentifier)?.Value;
+      var maNhanVien = (from a in _context.NhanViens
+                        where a.Username == userNameNhanVien
+                        select a.MaNhanVien
+      ).FirstOrDefault();
+      return Ok(maNhanVien);
+      // var hoaDon = (from a in _context.HoaDons
+      //               where a.MaHoaDon == maHoaDon
+      //               select a).FirstOrDefault();
+      // if (hoaDon == null) return BadRequest("Không tìm thấy hóa đơn ");
+      // var nhanVien = (from a in _context.NhanViens
+      //                 where a.MaNhanVien == maNhanVien
+      //                 select a).FirstOrDefault();
+      // if (nhanVien == null) return BadRequest("Không tìm thấy nhân viên ");
+      // if (trangThaiDon > 1 && trangThaiDon < -1) return BadRequest("Trạng thái đơn hàng cần cập nhật không hợp lệ");
+      // hoaDon.NgayChotDon = System.DateTime.Now;
+      // hoaDon.TinhTrangGiaoHang = trangThaiDon;
+      // _context.Entry(hoaDon).State = EntityState.Modified;
+      // try
+      // {
+      //   _context.SaveChanges();
 
-      }
-      catch (Exception ex)
-      {
-        return BadRequest(ex.InnerException.ToString());
-      }
-      return Ok("Đã cập nhật trạng thái đơn hàng thành công");
+      // }
+      // catch (Exception ex)
+      // {
+      //   return BadRequest(ex.InnerException.ToString());
+      // }
+      // return Ok("Đã cập nhật trạng thái đơn hàng thành công");
 
     }
 
     [HttpGet("ListDonHang")]
-    public ActionResult<List<dynamic>> ListDonHang(int status=0)
+    public ActionResult<List<dynamic>> ListDonHang(int status = 0)
     {
       var ListDonHang = (from a in _context.HoaDons
-                          where a.TinhTrangGiaoHang == status
+                         where a.TinhTrangGiaoHang == status
                          select new
                          {
                            maHoaDon = a.MaHoaDon,
-                           hoTen = (from a2 in _context.HoaDons join b2 in _context.KhachHangs on a2.MaKhachHang equals b2.MaKhachHang select b2.HoTen ).First(),
+                           hoTen = (from a2 in _context.HoaDons join b2 in _context.KhachHangs on a2.MaKhachHang equals b2.MaKhachHang select b2.HoTen).First(),
                            tinhTrang = (a.TinhTrangGiaoHang == 0 ? "Đang chờ duyệt" :
              (a.TinhTrangGiaoHang == 1 ? "Đang vận chuyển" : "Đã giao thành công")),
                            thoiGian = a.NgayChotDon,
@@ -126,30 +134,34 @@ namespace ShopLaptop_EFCore.Controllers.NhanVienController
     {
       var imageURL = Request.Scheme + "://" + Request.Host.Value + "/" + "Resources/Images/SanPham/";
       var chiTietHoaDon = (from a in _context.ChiTietHoaDons
-                                 join b in _context.HoaDons
-                                 on a.MaHoaDon equals b.MaHoaDon
-                                 join c in _context.SanPhams
-                                 on a.MaSanPham equals c.MaSanPham
-                                 where a.MaHoaDon == maHoaDon
-                                 select new
-                                 {
-                                   maSanPham = a.MaSanPham,
-                                   tenSanPham = c.TenSanPham,
-                                   soLuong = a.SoLuong,
-                                   donGia = (from d in _context.BienDongGia
-                                             where d.MaSanPham == a.MaSanPham
-                                             orderby d.LanThayDoiGia ascending
-                                             select d.GiaNhap * (1 + d.ChietKhau)).Last(),
-                                   anhSanPham = (from e in _context.AnhSanPhams
-                                                 where e.MaSanPham == a.MaSanPham
-                                                 select imageURL + e.FileAnh.Trim()).First()
-                                 }).ToList();
-      var trangThaiDon = (from a in _context.HoaDons where a.MaHoaDon == maHoaDon select new {
-        trangThai = (a.TinhTrangGiaoHang == -1 ? "Bị hủy" : a.TinhTrangGiaoHang == 0 ? "Đang chờ duyệt" : "Giao hàng thành công" )
-      }).FirstOrDefault();
-        
-      if (chiTietHoaDon.Count()==0) return BadRequest("Đơn hàng này không tồn tại");
-      return Ok(new {
+                           join b in _context.HoaDons
+                           on a.MaHoaDon equals b.MaHoaDon
+                           join c in _context.SanPhams
+                           on a.MaSanPham equals c.MaSanPham
+                           where a.MaHoaDon == maHoaDon
+                           select new
+                           {
+                             maSanPham = a.MaSanPham,
+                             tenSanPham = c.TenSanPham,
+                             soLuong = a.SoLuong,
+                             donGia = (from d in _context.BienDongGia
+                                       where d.MaSanPham == a.MaSanPham
+                                       orderby d.LanThayDoiGia ascending
+                                       select d.GiaNhap * (1 + d.ChietKhau)).Last(),
+                             anhSanPham = (from e in _context.AnhSanPhams
+                                           where e.MaSanPham == a.MaSanPham
+                                           select imageURL + e.FileAnh.Trim()).First()
+                           }).ToList();
+      var trangThaiDon = (from a in _context.HoaDons
+                          where a.MaHoaDon == maHoaDon
+                          select new
+                          {
+                            trangThai = (a.TinhTrangGiaoHang == -1 ? "Bị hủy" : a.TinhTrangGiaoHang == 0 ? "Đang chờ duyệt" : "Giao hàng thành công")
+                          }).FirstOrDefault();
+
+      if (chiTietHoaDon.Count() == 0) return BadRequest("Đơn hàng này không tồn tại");
+      return Ok(new
+      {
         trangThaiDon = trangThaiDon,
         chiTietHoaDon = chiTietHoaDon
       });
