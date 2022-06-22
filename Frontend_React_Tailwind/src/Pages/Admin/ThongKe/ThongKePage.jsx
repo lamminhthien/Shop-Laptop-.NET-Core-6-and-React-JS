b87f78c486c18820a51623f07b4f22c40956950a
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-06-22 17:54:06 +0700

    Update ThongKePage.jsx

--- Frontend_React_Tailwind/src/Pages/Admin/ThongKe/ThongKePage.jsx
+++ Frontend_React_Tailwind/src/Pages/Admin/ThongKe/ThongKePage.jsx
@@ -177,6 +177,15 @@ export default function ThongKePage() {
             </div>
           </div>
         </section>
+        <div className='content-header pt-4 pr-2'>
+          <div className='container pl-2 pr-2 w-full mx-auto'>
+            <div className='flex mx-[-7.5px] flex-wrap mb-2'>
+              <div className=' px-2'>
+                <h1 className='m-0 font-medium leading-3 text-xl'>Biểu đồ</h1>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
